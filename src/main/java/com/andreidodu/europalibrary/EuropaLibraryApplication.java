4d82facf65449fa208e5104855865d93063881cb
Andrei Dodu <andrei.dodu@hotmail.it>
2024-03-10 21:55:58 +0100

    first version

---
+++ src/main/java/com/andreidodu/europalibrary/EuropaLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.andreidodu.europalibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuropaLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EuropaLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andreidodu/europalibrary/EuropaLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andreidodu.europalibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuropaLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
