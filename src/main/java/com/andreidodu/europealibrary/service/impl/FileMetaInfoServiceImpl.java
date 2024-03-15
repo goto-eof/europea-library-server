b246ffb497806019d85587ba44f3671496e6837a
Andrei Dodu <andrei.dodu@hotmail.it>
2024-03-15 08:48:50 +0100

    feature: definition of main services of file meta-info module

---
+++ src/main/java/com/andreidodu/europealibrary/service/impl/FileMetaInfoServiceImpl.java
@@ -0,0 +1,12 @@
+package com.andreidodu.europealibrary.service.impl;
+
+import com.andreidodu.europealibrary.service.FileMetaInfoService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Transactional
+@RequiredArgsConstructor
+public class FileMetaInfoServiceImpl implements FileMetaInfoService {
+}
---
+++ src/main/java/com/andreidodu/europealibrary/service/impl/TagServiceImpl.java
@@ -0,0 +1,12 @@
+package com.andreidodu.europealibrary.service.impl;
+
+import com.andreidodu.europealibrary.service.TagService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Transactional
+@RequiredArgsConstructor
+public class TagServiceImpl implements TagService {
+}
