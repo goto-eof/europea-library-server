51a14c9f7f2dbb2361ca23b54b3e5cf3101aa2ff
Andrei Dodu <andrei.dodu@hotmail.it>
2024-03-16 20:18:14 +0100

    feature: added QuerydslPredicateExecutor

--- src/main/java/com/andreidodu/europealibrary/repository/common/TransactionalRepository.java
+++ src/main/java/com/andreidodu/europealibrary/repository/common/TransactionalRepository.java
@@ -1,9 +1,10 @@
 package com.andreidodu.europealibrary.repository.common;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.NoRepositoryBean;
 
 @NoRepositoryBean
-public interface TransactionalRepository<T, V> extends JpaRepository<T, V> {
+public interface TransactionalRepository<T, V> extends JpaRepository<T, V>, QuerydslPredicateExecutor<T> {
 }
 
