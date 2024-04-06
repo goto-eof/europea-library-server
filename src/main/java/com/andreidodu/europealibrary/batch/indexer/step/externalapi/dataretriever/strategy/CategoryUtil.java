0bd18d415252011a1a26221ab01f5772816af741
Andrei Dodu <andrei.dodu@hotmail.it>
2024-04-06 08:45:46 +0200

    fix: categories saving

--- src/main/java/com/andreidodu/europealibrary/batch/indexer/step/externalapi/dataretriever/strategy/CategoryUtil.java
+++ src/main/java/com/andreidodu/europealibrary/batch/indexer/step/externalapi/dataretriever/strategy/CategoryUtil.java
@@ -2,51 +2,39 @@ package com.andreidodu.europealibrary.batch.indexer.step.externalapi.dataretriev
 
 import com.andreidodu.europealibrary.model.Category;
 import com.andreidodu.europealibrary.repository.CategoryRepository;
-import com.andreidodu.europealibrary.repository.FileMetaInfoRepository;
-import com.andreidodu.europealibrary.util.StringUtil;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.LockModeType;
-import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class CategoryUtil {
     private final CategoryRepository categoryRepository;
-    private final FileMetaInfoRepository fileMetaInfoRepository;
-    @PersistenceContext
-    private final EntityManager entityManager;
 
+    @Retryable(retryFor = {DataIntegrityViolationException.class, NullPointerException.class}, maxAttemptsExpression = "1000000")
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public Category createCategoryEntity(String categoryName) {
-        try {
-            entityManager.createQuery("select l from Category l where lower(l.name) like lower(:name)", Category.class)
-                    .setParameter("name", categoryName)
-                    .setLockMode(LockModeType.PESSIMISTIC_WRITE)
-                    .getResultList().stream().findFirst()
-                    .ifPresentOrElse(entityManager::persist
-                            , () -> {
-                                String trimed = StringUtil.cleanAndTrimToNull(categoryName);
-                                if (trimed != null) {
-                                    trimed = trimed.toLowerCase();
-                                    entityManager.persist(createCategoryFromName(trimed));
-                                }
-                            });
-
-        } catch (Exception e) {
-            log.error("\n\n\n\nERROR: {}\n\n\n\n", e.getMessage());
+        Optional<Category> tagOptional = this.categoryRepository.findByNameIgnoreCase(categoryName);
+        Category category;
+        if (tagOptional.isEmpty()) {
+            category = createCategoryFromName(categoryName);
+            category = this.categoryRepository.save(category);
+            this.categoryRepository.flush();
+        } else {
+            category = tagOptional.get();
         }
-        return entityManager.createQuery("select l from Category l where lower(l.name) like lower(:name)", Category.class)
-                .setParameter("name", categoryName)
-                .setLockMode(LockModeType.NONE)
-                .getResultList().stream().findFirst().get();
+        if (category == null || category.getId() == null) {
+            throw new NullPointerException();
+        }
+        return category;
     }
-
 
     private static Category createCategoryFromName(String categoryName) {
         Category category = new Category();
--- src/main/java/com/andreidodu/europealibrary/batch/indexer/step/metainfo/dataextractor/strategy/TagUtil.java
+++ src/main/java/com/andreidodu/europealibrary/batch/indexer/step/metainfo/dataextractor/strategy/TagUtil.java
@@ -1,20 +1,12 @@
 package com.andreidodu.europealibrary.batch.indexer.step.metainfo.dataextractor.strategy;
 
-import com.andreidodu.europealibrary.exception.ApplicationException;
 import com.andreidodu.europealibrary.model.Tag;
-import com.andreidodu.europealibrary.repository.FileMetaInfoRepository;
 import com.andreidodu.europealibrary.repository.TagRepository;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.LockModeType;
-import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.StaleStateException;
-import org.hibernate.engine.jdbc.spi.SqlExceptionHelper;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,9 +17,6 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class TagUtil {
     private final TagRepository tagRepository;
-    private final FileMetaInfoRepository fileMetaInfoRepository;
-    @PersistenceContext
-    private final EntityManager entityManager;
 
     @Retryable(retryFor = {DataIntegrityViolationException.class, NullPointerException.class}, maxAttemptsExpression = "1000000" )
     @Transactional(propagation = Propagation.REQUIRES_NEW)
@@ -43,22 +32,6 @@ public class TagUtil {
         }
         if (tag == null || tag.getId() == null) {
             throw new NullPointerException();
-        }
-        return tag;
-    }
-
-    @Transactional(propagation = Propagation.REQUIRES_NEW, isolation = Isolation.REPEATABLE_READ)
-    public Tag createTagEntityWorking(String tagName) {
-        Optional<Tag> tagOptional = this.tagRepository.findByNameIgnoreCase(tagName);
-        Tag tag;
-        if (tagOptional.isEmpty()) {
-            tag = createTagFromName(tagName);
-            entityManager.persist(tag);
-            tag = this.tagRepository.findByNameIgnoreCase(tagName).get();
-            this.entityManager.lock(tag, LockModeType.PESSIMISTIC_WRITE);
-            this.entityManager.flush();
-        } else {
-            tag = tagOptional.get(); //this.tagRepository.findByNameIgnoreCase(tagName).get();
         }
         return tag;
     }
