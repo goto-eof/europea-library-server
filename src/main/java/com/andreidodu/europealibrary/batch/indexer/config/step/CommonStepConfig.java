04fe87a1a6243ce842429ef3417cbbd510de72b7
Andrei Dodu <andrei.dodu@hotmail.it>
2024-04-06 22:42:43 +0200

    feature: configurable multi-thread pool size

--- src/main/java/com/andreidodu/europealibrary/batch/indexer/config/step/CommonStepConfig.java
+++ src/main/java/com/andreidodu/europealibrary/batch/indexer/config/step/CommonStepConfig.java
@@ -1,6 +1,7 @@
 package com.andreidodu.europealibrary.batch.indexer.config.step;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -8,15 +9,35 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @Slf4j
 @Configuration
 public class CommonStepConfig {
+
+
+    @Value("${com.andreidodu.europea-library.core-pool-size}")
+    private Integer corePoolSize;
+    @Value("${com.andreidodu.europea-library.max-pool-size}")
+    private Integer maxPoolSize;
+
     @Bean("threadPoolTaskExecutor")
     public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
-        int processors = Runtime.getRuntime().availableProcessors() - 1;
-        log.debug("\n\n\n====================\nYou have {} processors available for processing\n=====================\n\n\n", processors);
+        int corePoolSize = Runtime.getRuntime().availableProcessors() - 1;
+        int maxPoolSize = corePoolSize * 2;
+
+        if (isPoolSizeValidValue(this.corePoolSize)) {
+            corePoolSize = this.corePoolSize;
+        }
+        if (isPoolSizeValidValue(this.maxPoolSize)) {
+            maxPoolSize = this.maxPoolSize;
+        }
+
+        log.debug("\n====================\nI will use {} as corePoolSize and {} as maxPoolSize for processing\n=====================\n", corePoolSize, maxPoolSize);
         ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
-        taskExecutor.setMaxPoolSize(processors);
-        taskExecutor.setCorePoolSize(processors);
+        taskExecutor.setCorePoolSize(corePoolSize);
+        taskExecutor.setMaxPoolSize(maxPoolSize);
         taskExecutor.setWaitForTasksToCompleteOnShutdown(true);
         taskExecutor.afterPropertiesSet();
         return taskExecutor;
+    }
+
+    private boolean isPoolSizeValidValue(Integer poolSize) {
+        return poolSize != null && poolSize > 0;
     }
 }
