193a897a92a52841ebc22cdedbe6d4eccbaebce5
Andrei Dodu <andrei.dodu@hotmail.it>
2024-04-24 22:42:56 +0200

    feature: security layer - proceeding

--- src/main/java/com/andreidodu/europealibrary/EuropeaLibraryApplication.java
+++ src/main/java/com/andreidodu/europealibrary/EuropeaLibraryApplication.java
@@ -1,13 +1,16 @@
 package com.andreidodu.europealibrary;
 
+import com.andreidodu.europealibrary.config.auth.RsaKeyConfigProperties;
 import com.andreidodu.europealibrary.util.PostApplicationRunUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
 @RequiredArgsConstructor
+@EnableConfigurationProperties(RsaKeyConfigProperties.class)
 public class EuropeaLibraryApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/andreidodu/europealibrary/mapper/AuthUserMapper.java
@@ -0,0 +1,19 @@
+package com.andreidodu.europealibrary.mapper;
+
+import com.andreidodu.europealibrary.dto.auth.AuthUserDTO;
+import com.andreidodu.europealibrary.model.auth.User;
+import lombok.extern.slf4j.Slf4j;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+@Slf4j
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR)
+public abstract class AuthUserMapper {
+
+    public abstract User toModel(AuthUserDTO dto);
+
+    @Mapping(ignore = true, target = "authorities")
+    public abstract AuthUserDTO toDTO(User model);
+
+}
