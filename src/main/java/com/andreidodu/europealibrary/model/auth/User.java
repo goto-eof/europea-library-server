d59d00bd46d556fd2a0ddf0abea19d8b8f382022
Andrei Dodu <andrei.dodu@hotmail.it>
2024-04-24 22:17:33 +0200

    feature: security layer - tables

--- src/main/java/com/andreidodu/europealibrary/model/auth/User.java
+++ src/main/java/com/andreidodu/europealibrary/model/auth/User.java
@@ -1,6 +1,8 @@
-package com.andreidodu.europealibrary.model.user;
+package com.andreidodu.europealibrary.model.auth;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.NonNull;
 
 @Entity
 @Table(name = "el_user", uniqueConstraints = {@UniqueConstraint(columnNames = "username,email")})
@@ -11,8 +13,12 @@ public class User {
     @SequenceGenerator(name = "el_user_seq", sequenceName = "el_user_seq", allocationSize = 50)
     private Long id;
 
+    @Column(name = "username", unique = true)
     private String username;
+    @Column(name = "email", unique = true)
     private String email;
+    @NonNull
+    @JsonIgnore
     private String password;
 
 }
---
+++ src/main/java/com/andreidodu/europealibrary/repository/auth/UserRepository.java
@@ -0,0 +1,12 @@
+package com.andreidodu.europealibrary.repository.auth;
+
+import com.andreidodu.europealibrary.model.auth.User;
+import com.andreidodu.europealibrary.repository.common.TransactionalRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends TransactionalRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
