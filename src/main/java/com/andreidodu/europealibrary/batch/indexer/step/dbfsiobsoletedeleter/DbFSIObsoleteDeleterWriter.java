ce6e1989c3665018162b056db5ce402ec1747a7a
Andrei Dodu <andrei.dodu@hotmail.it>
2024-05-05 18:27:12 +0200

    fix: delete featured before file system item

--- src/main/java/com/andreidodu/europealibrary/batch/indexer/step/dbfsiobsoletedeleter/DbFSIObsoleteDeleterWriter.java
+++ src/main/java/com/andreidodu/europealibrary/batch/indexer/step/dbfsiobsoletedeleter/DbFSIObsoleteDeleterWriter.java
@@ -1,21 +1,34 @@
 package com.andreidodu.europealibrary.batch.indexer.step.dbfsiobsoletedeleter;
 
+import com.andreidodu.europealibrary.model.FeaturedFileSystemItem;
+import com.andreidodu.europealibrary.model.FileSystemItem;
+import com.andreidodu.europealibrary.repository.CustomFeaturedFileSystemItemRepository;
+import com.andreidodu.europealibrary.repository.FeaturedFileSystemRepository;
 import com.andreidodu.europealibrary.repository.FileSystemItemRepository;
+import com.andreidodu.europealibrary.resource.FeaturedFileSystemItemResource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.item.Chunk;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class DbFSIObsoleteDeleterWriter implements ItemWriter<Long> {
     final private FileSystemItemRepository fileSystemItemRepository;
+    final private FeaturedFileSystemRepository featuredFileSystemRepository;
 
     @Override
     public void write(Chunk<? extends Long> chunk) {
-        this.fileSystemItemRepository.deleteAllByIdInBatch((Iterable<Long>) chunk.getItems());
+        chunk.getItems().forEach(id -> {
+            FileSystemItem fileSystemItem = this.fileSystemItemRepository.findById(id).get();
+            Optional.ofNullable(fileSystemItem.getFeaturedFileSystemItem())
+                    .ifPresent(this.featuredFileSystemRepository::delete);
+            this.fileSystemItemRepository.delete(fileSystemItem);
+        });
         this.fileSystemItemRepository.flush();
         log.debug("deleted {} records", chunk.getItems().size());
     }
