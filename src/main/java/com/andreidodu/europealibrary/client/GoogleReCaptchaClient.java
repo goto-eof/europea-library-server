ac06c7d8c7114b8b22f4d8676a4149bd87af64ad
Andrei Dodu <andrei.dodu@hotmail.it>
2024-06-01 11:27:33 +0200

    [#73] feature: customizable captcha uri

--- src/main/java/com/andreidodu/europealibrary/client/GoogleReCaptchaClient.java
+++ src/main/java/com/andreidodu/europealibrary/client/GoogleReCaptchaClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value = "google-recaptcha", url = "www.google.com/recaptcha/api/siteverify")
+@FeignClient(value = "google-recaptcha", url = "${com.andreidodu.europea-library.google.recaptcha.query-url}")
 public interface GoogleReCaptchaClient {
     @RequestMapping(method = RequestMethod.POST)
     GoogleRecaptchaResponseDTO verify(@RequestBody GoogleRecaptchaDTO googleRecaptchaDTO);
