90950465a3ff16e92c9636a842245da1dca6deae
Andrei Dodu <andrei.dodu@hotmail.it>
2024-06-06 08:12:04 +0200

    [#84] feature: website e-books clonning protection

---
+++ src/main/java/com/andreidodu/europealibrary/repository/TemporaryResourceIdentifierRepository.java
@@ -0,0 +1,7 @@
+package com.andreidodu.europealibrary.repository;
+
+import com.andreidodu.europealibrary.model.TemporaryResourceIdentifier;
+import com.andreidodu.europealibrary.repository.common.TransactionalRepository;
+
+public interface TemporaryResourceIdentifierRepository extends TransactionalRepository<TemporaryResourceIdentifier, Long> {
+}
